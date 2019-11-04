6411dfe626867d0887fec916a32d379d23e0e427
megatron <1251783361@qq.com>
2019-11-04 14:19:17 +0800

    adjust

--- log_upload.sh
+++ log_upload.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PHP_LOG_PATH='/www/server/php/56/var/log/php-fpm.log'
 NGINX_LOG_PATH='/www/wwwlogs/xcx.milinzone.com.error.log'
+DING_TALK_URL='https://oapi.dingtalk.com/robot/send?access_token=40f602f64fd739b9f2140e628f4a1e3468e9bb803e3ebde7d15d8cc579b7f2d5'
 
 
 php_log_process(){
@@ -28,14 +29,24 @@ main(){
     fi
 }
 
+test(){
+    message=`sed -n '/2019\/10\/25\s18:[0-9][0-9]:[0-9][0-9]\s\[error\]/ p' $NGINX_LOG_PATH`
+    echo ${message//\"/-}
+    push ${message//\"/-}
+}
+
 push(){
-    curl 'https://oapi.dingtalk.com/robot/send?access_token=40f602f64fd739b9f2140e628f4a1e3468e9bb803e3ebde7d15d8cc579b7f2d5' \
+    curl '"$DING_TALK_URL"' \
        -H 'Content-Type: application/json' \
        -d '{"msgtype": "text",
             "text": {
-                 "content": "'"$1"'"
+                 "content": "'"$*"'"
             }
           }'
 }
 
-main
+if [[ $1 == 'test' ]];then
+    test
+else
+    main
+fi
